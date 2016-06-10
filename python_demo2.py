c403cab68e5d69b8aa967d13f37adb92b1b2efb1
Selinallg <liu.lg@163.com>
2016-06-09 22:29:22 -0500

    Delete python_demo2.py
    移除 添加

--- python_demo2.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-# coding = utf-8
-
-def  change( mylist):
-	''' xiugai chuangru canshu liebiao jiba 哈哈终于可以支持中文 设的发生方式 几把'''
-	mylist.append([1,2,3,4]);
-	print "new mylist  --->",mylist
-	return
- 
